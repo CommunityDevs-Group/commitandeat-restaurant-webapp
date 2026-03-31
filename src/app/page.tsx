67860f22adcb31e8d4b98bf29d2c047790f5fff1
Ugur Huseynaliyev <huseyneliyevugur06@gmail.com>
2026-03-31 21:35:30 +0400

    feat: add About Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,6 @@ import HeroSection from "@/components/sections/HeroSection";
 export default function Home() {
   return (
     <main>
-      {/* <h1>Commit &amp; Eat</h1>
-      <p>Landing page – coming soon.</p> */}
       <HeroSection />
       <AboutSection />
     </main>
